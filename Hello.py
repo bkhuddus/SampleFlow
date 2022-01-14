a028bb085be2a26c402262aeacd57a2d2c968ebd
bkhuddus <97620183+bkhuddus@users.noreply.github.com>
2022-01-14 10:44:46 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,15 +1,7 @@
-from prefect import task, Flow, Parameter
-
-@task(log_stdout=True)
 def say_hello(name):
     print("Hello, {}!".format(name))
 
-with Flow("My First Flow") as flow:
-    name = Parameter('name')
-    say_hello(name)
-
-flow.run(name='Python')
-flow.run(name='Bash')
+say_hello('bash')
 
 
 
